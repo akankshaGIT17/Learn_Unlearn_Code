dda8a662dde60a1e2fce4365eeb4978930de0170
akankshGIT17 <akanksha.saxena16@gmail.com>
2022-05-22 20:29:41 +0530

    added with vs code

--- PalindromeString.cs
+++ PalindromeString.cs
@@ -1,33 +1,36 @@
 using System;
-public class PalindromeString{
- public bool Palindrome(string str)
+public class PalindromeString
+{
+    public bool Palindrome(string str)
     {
-         // Pointers pointing to the beginning
+        // Pointers pointing to the beginning
         // and the end of the string
         int i = 0, j = str.Length - 1;
- 
+
         // While there are characters to compare
-        while (i < j) {
- 
+        while (i < j)
+        {
+
             // If there is a mismatch
             if (str[i] != str[j])
                 return false;
- 
+
             // Increment first pointer and
             // decrement the other
             i++;
             j--;
         }
- 
+
         // Given string is a palindrome
         return true;
     }
     public void Caller()
     {
-        Console.WriteLine("madam - " + Palindrome("madam"));
-       Console.WriteLine("maam - " + Palindrome("maam"));
-       Console.WriteLine("nun - " + Palindrome("nun"));
-       Console.WriteLine("aabbaa - " + Palindrome("aabbaa"));
-             Console.WriteLine("abcabc - " + Palindrome("abcabc"));
+        string[] palindromeString = { "abba", "madam", "chuku", "nun", "abcabc", "maam" };
+        foreach (var str in palindromeString)
+        {
+            
+            Console.WriteLine(str+ ": "+ Palindrome(str));
+        }
     }
 }
